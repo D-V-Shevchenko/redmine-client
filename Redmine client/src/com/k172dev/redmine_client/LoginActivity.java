ad17d2cbfd6d084f715ceff30da9ee83e7eb828e
Dmitriy Shevchenko <D.V.Shevch@gmail.com>
2012-12-18 23:00:32 +0400

    Empty Main activity has been added

--- Redmine client/src/com/k172dev/redmine_client/LoginActivity.java
+++ Redmine client/src/com/k172dev/redmine_client/LoginActivity.java
@@ -2,14 +2,33 @@ package com.k172dev.redmine_client;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
 
+	Context ctx = null;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        ctx = this;
+        
+        Button tempBtn = (Button) findViewById(R.id.button1);
+        tempBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(ctx, MainActivity.class);
+				startActivity(intent);
+			}
+		});
     }
 
     @Override
